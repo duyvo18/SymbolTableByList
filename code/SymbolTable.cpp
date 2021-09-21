1eb42e596ae6b59c403b2b689b075a40c220253b
Thiên Phú <>
2021-09-21 17:42:58 +0000

    Implemented, awaiting for debug

--- code/SymbolTable.cpp
+++ code/SymbolTable.cpp
@@ -1,7 +1,136 @@
 #include "SymbolTable.h"
 
+const int lowercaseLowerBound = 97;
+const int lowercaseUpperBound = 122;
+const int uppercaseLowerBound = 65;
+const int uppercaseUpperBound = 90;
+const int numericLowerBound = 48;
+const int numericUpperBound = 57;
+const int space = 32;
+const int underscore = 95;
+const int apostrophe = 39;
+
 template <typename T>
 void SymbolTable<T>::run(string filename)
 {
-    cout << "success";
+    ifstream fin;
+    fin.open(filename); 
+    
+    while(!fin.eof())
+    {
+        string ins;
+        string temp;
+        getline(fin, ins, '\n');
+        
+        //1 ve
+        if(ins == "BEGIN")
+        {
+            BEGIN();
+            continue;
+        }
+        else if(ins == "END")
+        {
+            if(!END())
+                throw UnknownBlock();
+            else
+                continue;
+        }
+        // n ve    
+        int pos = ins.find_first_of(' ', 0);
+        temp = ins.substr(0, pos);
+        //https://www.cplusplus.com/reference/string/string/
+
+        if(temp == "INSERT")
+        {
+            //cutting
+            temp = ins.subtr(pos + 1, ins.find_first_of(' ', pos + 1));
+            pos = ins.find_first_of(' ', pos + 1);
+
+            //valid name
+            if(temp[0] >= lowercaseLowerBound && temp[0] <= lowercaseUpperBound)
+            {
+                for(int i = 1; i <= temp.length(); i++)
+                {
+                    if(temp[i] >= lowercaseLowerBound && temp[i] <= lowercaseUpperBound); 
+                    else if(temp[i] >= numericLowerBound && temp[i] <= numericUpperBound);
+                    else if(temp[i] == underscore);
+                    else
+                        throw InvalidInstruction(ins);
+                }
+            }
+            else 
+                throw InvalidInstruction(ins);
+
+            //cutting
+            temp = ins.subtr(pos + 1, ins.find_first_of('\n', pos + 1));
+            pos = ins.find_first_of('\n', pos + 1);
+
+            //valid datatype
+            if(temp == "number" || temp == "string");
+            else
+                throw InvalidInstruction(ins);
+            
+        }
+        else if(ins == "ASSIGN");
+        {
+            int scenario = 0;
+
+            //cutting
+            temp = ins.subtr(pos + 1, ins.find_first_of(' ', pos + 1));
+            pos = ins.find_first_of(' ', pos + 1);
+
+            //valid name
+            if(temp[0] >= lowercaseLowerBound && temp[0] <= lowercaseUpperBound)
+            {
+                for(int i = 1; i <= temp.length(); i++)
+                {
+                    if(temp[i] >= lowercaseLowerBound && temp[i] <= lowercaseUpperBound); 
+                    else if(temp[i] >= numericLowerBound && temp[i] <= numericUpperBound);
+                    else if(temp[i] == underscore);
+                    else
+                        throw InvalidInstruction(ins);
+                }
+            }
+            else 
+                throw InvalidInstruction(ins);
+            
+            //type mismatch
+            //cutting
+            temp = ins.subtr(pos + 1, ins.find_first_of('\n', pos + 1));
+            pos = ins.find_first_of('\n', pos + 1);    
+
+            if(temp.back() == apostrophe && temp.front() == apostrophe)
+                throw InvalidInstruction(ins);
+            else
+                for(int i = 1; i <= temp.length(); i++)
+                {
+                    if(temp[i] >= lowercaseLowerBound && temp[i] <= lowercaseUpperBound); 
+                    else if(temp[i] >= numericLowerBound && temp[i] <= numericUpperBound);
+                    else if(temp[i] >= uppercaseLowerBound && temp[i] <= uppercaseUpperBound);
+                    else if(temp[i] == space);
+                    else
+                        throw InvalidInstruction(ins);
+                }
+            
+            ASSIGN(name, datatype);
+
+            //controller
+            if(scenario == 1)
+                throw InvalidInstruction(ins);
+            else if(scenario == 2)
+                throw TypeMismatch(ins);
+            else 
+                return "successs\n";
+
+
+            //undeclared 
+        }
+        else if(ins == "BEGIN");
+        else if(ins == "BEGIN");
+        else if(ins == "BEGIN");
+        else    
+            throw InvalidInstruction();
+    }
+
+    // check
 }
--- code/SymbolTable.h
+++ code/SymbolTable.h
@@ -3,6 +3,17 @@
 #include "main.h"
 
 
+const int lowercaseLowerBound = 97;
+const int lowercaseUpperBound = 122;
+const int uppercaseLowerBound = 65;
+const int uppercaseUpperBound = 90;
+const int numericLowerBound = 48;
+const int numericUpperBound = 57;
+const int space = 32;
+const int underscore = 95;
+const int apostrophe = 39;
+
+
 
 template <typename T>
 class SymbolTable
@@ -12,9 +23,8 @@ private:
     class Scope;
 
     Scope* scopeHead;
-    Scope* scopeCurrent;
-    int levelIndex;
-
+    Scope* scopeCurrent; // pointer to current scope
+    int level; // current scope index
 
     class Symbol
     {
@@ -23,14 +33,16 @@ private:
         string datatype;
         T data;
         Symbol* symbolNext;
+        Symbol* symbolPrev;
     public:
         Symbol(string name, string datatype)
         {
             this->name = name;
             this->datatype = datatype;
             symbolNext = NULL;
-        } 
-
+            symbolPrev = NULL;
+        }
+        
         void setName(string name)
         {
             this->name = name;
@@ -39,6 +51,21 @@ private:
         void setDatatype(string datatype) 
         { 
             this->datatype = datatype;
+        }
+
+        void setData(T data)
+        {
+           this->data = data;
+        }
+
+        void setNext(Symbol* symbol)
+        {
+          symbolNext = symbol;
+        }
+
+        void setPrev(Symbol* symbol)
+        {
+          symbolPrev = symbol;
         }
 
         string getName()
@@ -55,6 +82,16 @@ private:
         {
             return this->data;
         }
+
+        Symbol* getNext()
+        {
+            return symbolNext;
+        }
+
+        Symbol* getPrev()
+        {
+            return symbolPrev;
+        }
     };
 
     class Scope
@@ -63,43 +100,263 @@ private:
         Symbol* symbolHead;
         Symbol* symbolCurrent;
         Scope* scopeNext;
-        int level;
+        Scope* scopePrev;
     public:
         Scope()
         {
-            symbolHead = new Symbol();
+            symbolHead = NULL;
             symbolCurrent = NULL;
-            symbolNext = NULL;
-            level = levelIndex;
+            scopeNext = NULL;
+            scopePrev - NULL;
         }
-        // void addNewScope()
-        // int findCurrentScopeLevel()
-        // void addSymbolToScope(...)
-    };
 
+        ~Scope()
+        {
+            while(symbolCurrent != NULL)
+            {
+                Symbol* temp = symbolCurrent;
 
-// what the current pointers are for Kuo ? -_- 
-    
+                symbolCurrent = symbolCurrent->getPrev(); 
+                symbolCurrent->getNext() = NULL;
+
+                delete temp;
+            }
+        }
+
+        void addSymbol(string name, string datatype)
+        {            
+            Symbol newSymbol = new Symbol(name, datatype);
+
+            if (symbolHead == NULL)
+            {
+              symbolHead = newSymbol;
+              symbolCurrent = newSymbol;
+            }
+            else
+            {
+              symbolCurrent.setNext(newSymbol);
+              newSymbol.setPrev(symbolCurrent);
+
+              symbolCurrent = newSymbol;
+            }
+        }
+
+        bool assignSymbol(string name, T data)
+        {
+            Symbol* temp = symbolHead;
+
+            while(temp != NULL)
+            {
+              if (temp.getName() == name)
+              {
+                temp.setData(data);
+                return true;
+              }
+              else
+              {
+                temp = temp.getNext();
+              }
+            }
+
+            return false;
+        }
+
+        bool findSymbol(string name)
+        {
+            Symbol* temp = symbolHead;
+
+            while (temp != NULL)
+            {
+              if (temp.getName() != name)
+              {
+                temp = temp.getNext();
+              }
+              else
+              {
+                return true
+              }
+            }
+            
+            return false;
+        }        
+
+        void setNext(Scope* scope)
+        {
+          scopeNext = scope;
+        }
+
+        void setPrev(Scope* scope)
+        {
+          scopeNext = scope;
+        }
+
+        Scope* getNext()
+        {
+          return scopeNext;
+        }
+
+        Scope* getPrev()
+        {
+          return scopePrev;
+        }
+
+    };    
 
 public:
     SymbolTable()
     {
         levelIndex = 0;
         scopeHead = new Scope();
-        scopeCurrent = NULL;
-        scopeNext = NULL;
+        scopeCurrent = scopeHead;
+    }
+
+    ~SymbolTable()
+    {
+        while(scopeCurrent != scopeHead)
+        {
+            END();
+        }
+        
+        Scope* temp = scopeCurrent;
+
+        scopeCurrent = scopeCurrent->getPrev();
+        scopeCurrent->getNext() = NULL;
+
+        delete temp;
     }
 
     void run(string filename);
 
-    void INSERT(string name, string datatype)
+    void addScope()
     {
+        Scope newScope = new Scope();
 
+        scopeCurrent.setNext(newScope);
+        newScope.setPrev(scopeCurrent);
+
+        scopeCurrent = newScope;
+        ++levelIndex;
     }
 
-    void ASSIGN(string name, T value)
+    bool INSERT(string name, string datatype)
+    {
+        if(scopeCurrent->findSymbol(name))
+            return false;
+        scopeCurrent.addSymbol(name, datatype);
+        return true;
+        
+    }
+
+    int ASSIGN(string name, T value)
+    {
+        if(scopeCurrent->findSymbol(name))
+            return 1;     
+
+        Scope* temp = scopeCurrent;
+        
+        while (temp != NULL)
+        {
+            if (temp.findSymbol(name))
+            {
+                temp.assignSymbol(name, value);
+                return true;
+            }
+            else
+                temp = temp.getPrev();
+        }
+        
+        return 0;
+    }
+
+    int LOOKUP(string name)
+    {
+        Scope* temp = scopeCurrent;
+        for(int i = levelIndex; i == 0; i--)
+        {
+            if(temp.findSymbol(name))
+                return i;
+            temp = temp->getPrev;
+        }
+        
+        return -1; 
+    }
+
+    
+    string PRINT()
+    {
+        Scope* scopeT = scopeCurrent;
+        Scope temp; 
+        string print = "";
+
+        for(int i = levelIndex; i >= 0; i--)
+        {
+            // per scope
+            Symbol* symbolT = scopeT->symbolCurrent;
+            while(symbolT->getPrev() != NULL)
+            {
+                // per symbol in scope
+                if(temp.findSymbol(symbolT.getName()))
+                    temp.addSymbol(symbolT.getName,symbolT.getDatatype);
+                    print = print + symbolT.getName() + "//" + i + " ";
+                SymbolT = SymbolT->getPrev();
+            }
+            
+            scopeT = scopeT->getPrev();
+        }
+
+        print.erase(print.length() - 1, 1);
+        return print;
+    }
+
+    string RPRINT()
     {
 
+        Scope* scopeT = scopeCurrent;
+        Scope temp; 
+        string rprint = "";
+
+        for(int i = levelIndex; i >= 0; i--)
+        {
+            // per scope
+            Symbol* symbolT = scopeT->symbolCurrent;
+            while(symbolT->getPrev() != NULL)
+            {
+                // per symbol in scope
+                if(temp.findSymbol(symbolT.getName()))
+                    temp.addSymbol(symbolT.getName,symbolT.getDatatype);
+                    rprint = symbolT.getName() + "//" + i + " " + rprint;
+                SymbolT = SymbolT->getPrev();
+            }
+            
+            scopeT = scopeT->getPrev();
+        }
+
+        rprint.erase(rprint.length() - 1, 1);
+        return rprint;
+    }
+
+    void BEGIN()
+    {
+        addScope();
+    }
+
+    bool END()
+    {
+        if(scopeCurrent != scopeHead)
+        {
+            Scope* temp = scopeCurrent;
+
+            scopeCurrent = scopeCurrent->getPrev();
+            scopeCurrent->getNext() = NULL;
+
+            levelIndex--;
+            delete temp;
+
+            return true;
+        }
+        else 
+            return false;
     }
 };
+
 #endif
